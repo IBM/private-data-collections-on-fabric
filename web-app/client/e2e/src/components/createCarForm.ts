6fde1b45db55fbff0b06e9606f14e954b5081965
Horea Porutiu <horeaporutiu@gmail.com>
2020-02-05 15:36:35 -0800

    rm angular ui

--- web-app/client/e2e/src/components/createCarForm.ts
+++
@@ -1,9 +0,0 @@
-import { browser, element, by, ExpectedConditions } from 'protractor';
-
-export class CreateCarForm {
-
-  static waitToAppear() {
-    return browser.wait(ExpectedConditions.visibilityOf(element(by.tagName('app-create-car-form'))), 5000);
-  }
-
-}
